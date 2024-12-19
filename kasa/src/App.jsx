feb57c66b010fb9ac27b1a5adb0669ce15ecf7be
asma benzerrouk <asma.benzerrouk@gmail.com>
2024-12-19 16:12:42 +0100

    mise à jours fichiers

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,10 +1,10 @@
+
 import React from "react"
 import { BrowserRouter as Router } from "react-router-dom"
 import RoutesApp from "./routes/routes"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 function App() {
-
   return (
     <Router>
       <Header/>
