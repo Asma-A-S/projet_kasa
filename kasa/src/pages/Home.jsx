ce0cbc2a08da9471776bb1d0c34b1665596872f7
asma benzerrouk <asma.benzerrouk@gmail.com>
2024-12-23 00:10:47 +0100

    chore: ajout link vers la page appart

--- kasa/src/pages/Home.jsx
+++ kasa/src/pages/Home.jsx
@@ -1,18 +1,20 @@
+import { Link } from "react-router-dom"
 import "../styles/Home.scss"
 import Banner from "../components/Banner"
 import data from "../data/data.json"
 import Cards from "../components/Cards"
 function Home(){
-    console.log(data)
 return(
 <>
     <Banner texte="Chez vous, partout et ailleurs"/>
     <section className="gallery">
       {data.map(({id, title, cover})=>(
         <div className="card" key={id}>
+          <Link to={`/appart/${id}`}>
             <Cards
              cover={cover}
              title ={title}/>
+          </Link>
         </div>
       ))}
     </section>
