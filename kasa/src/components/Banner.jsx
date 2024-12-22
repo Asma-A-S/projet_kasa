c2c8d6c3afb5547f525e28f6e2322f63b627f356
asma benzerrouk <asma.benzerrouk@gmail.com>
2024-12-22 17:15:15 +0100

    chore: ajout useLocation

--- kasa/src/components/Banner.jsx
+++ kasa/src/components/Banner.jsx
@@ -1,8 +1,12 @@
-import banner from "../assets/banner-home.png"
+
+import bannerHome from "../assets/banner-home.png"
+import bannerAbout from "../assets/banner-about.png"
+import { useLocation } from "react-router-dom"
 function Banner( {texte}){
+    const location = useLocation()
     return(
 <div className="banner">
-    <img src={banner} alt="image banner"/>
+    <img src={location.pathname != "/About" ? bannerHome: bannerAbout} alt="image banner"/>
     <h1 className="texte">{texte}</h1>
 </div>)
 }
