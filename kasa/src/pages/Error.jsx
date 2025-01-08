cde81e46bed7c137544d34dd75be9fce1edd81e0
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-01-08 17:34:17 +0100

    implémenter logique page erreur

--- kasa/src/pages/Error.jsx
+++ kasa/src/pages/Error.jsx
@@ -1,4 +1,17 @@
-function Error(){
-return(<h1>error</h1>)
+import { Link } from 'react-router-dom'
+import errorImg from '../assets/404.png'
+import '../styles/Error.scss'
+function Error() {
+    return (
+        <div className="error">
+            <img src={errorImg} alt="erreur 404" className="error_image" />
+            <h1 className="error_texte">
+                Oups! La page que vous demandez n'existe pas.
+            </h1>
+            <Link to="/" className="error_link">
+                Retourner sur la page d’accueil
+            </Link>
+        </div>
+    )
 }
 export default Error
