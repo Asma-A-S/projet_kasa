db6bfa9da88aa4bc5aabe04156d2d44018b5908f
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-01-13 17:09:40 +0100

    remplacer l'image 404 par un texte, ajout span à error_texte

--- kasa/src/pages/Error.jsx
+++ kasa/src/pages/Error.jsx
@@ -2,12 +2,13 @@ import { Link } from 'react-router-dom'
 import errorImg from '../assets/404.png'
 import '../styles/Error.scss'
 function Error() {
+    //<img src={errorImg} alt="erreur 404" className="error_image" />
     return (
         <div className="error">
-            <img src={errorImg} alt="erreur 404" className="error_image" />
-            <h1 className="error_texte">
-                Oups! La page que vous demandez n'existe pas.
-            </h1>
+            <h1 className="error_404">404</h1>
+            <p className="error_texte">
+                <span>Oups! La page que</span> vous demandez n'existe pas.
+            </p>
             <Link to="/" className="error_link">
                 Retourner sur la page d’accueil
             </Link>
