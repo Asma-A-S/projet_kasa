97f628dc0f6b11e185520e5c7910d08b934325e3
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-01-13 17:06:19 +0100

    Modifier le nom des classeName

--- kasa/src/components/Header.jsx
+++ kasa/src/components/Header.jsx
@@ -1,14 +1,18 @@
-import React from "react"
-import {NavLink} from "react-router-dom"
-import logo from "../assets/LOGO.png"
-import "../styles/Header.scss"
-function Header(){
-    return(
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import logo from '../assets/LOGO.png'
+import '../styles/Header.scss'
+function Header() {
+    return (
         <div className="header">
-            <img src={logo} alt="logo" className="logo"/>
-            <nav className="navbar">
-                <NavLink to="/" className="navbar-item">Accueil</NavLink>
-                <NavLink to="/About" className="navbar-item">A propos</NavLink>
+            <img src={logo} alt="header_logo" className="header_logo" />
+            <nav className="header_navbar">
+                <NavLink to="/" className="navbar-item">
+                    Accueil
+                </NavLink>
+                <NavLink to="/About" className="navbar-item">
+                    A propos
+                </NavLink>
             </nav>
         </div>
     )
