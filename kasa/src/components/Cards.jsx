48a1784b12fcff018151621429dbb09f25eaef4b
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-01-13 17:08:01 +0100

    formatage prettierc

--- kasa/src/components/Cards.jsx
+++ kasa/src/components/Cards.jsx
@@ -1,9 +1,9 @@
-function Cards ({cover, title}){
-    return(
-        <div className="card">
-            <img src ={cover} alt="photo de la location"/>
+function Cards({ cover, title }) {
+    return (
+        <>
+            <img src={cover} alt="photo de la location" />
             <h2 className="title">{title}</h2>
-        </div>
+        </>
     )
 }
 export default Cards
